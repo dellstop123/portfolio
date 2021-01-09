2ac296acf9eb47d958da5c208825c0689a32fe78
Guneet Singh <guneetsingh@Guneets-MacBook-Air.local>
2021-01-09 17:19:34 +0530

    First commit

---
+++ portfolio/src/components/introduction.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/guneet2.png)',backgroundSize:'length'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />I'm Guneet</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1_ThB5EUlV7XPByUD-zS8-LiDYx_DJvMghgAO_bWgiqQ/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/guneet2.png)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I love building<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dellstop123" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/guneet2.png)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I often <br/>Write ... </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://myblogbook.xyz/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ portfolio/src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Meegaan technologies <span>2019-present</span></h2>
+                        <p>I recently have joined the Meegaan technologies as a Software Developer-Intern in the Engineering Team. My major part of the work has been into the field of creating sustainable and flexible Code which enables developers at Meegaan technologies to do rapid development.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Masters at University of Jammu<span>2016-2019</span></h2>
+                        <p>I completed my masters with major in Computer Applications. I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2012-2016</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
